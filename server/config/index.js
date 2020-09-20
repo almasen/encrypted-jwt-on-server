0a0eaa2ebb3ad6a60a991edb3a11bb4843703177
Daniel Almasi <almd@pm.me>
2020-09-20 18:27:17 +0100

    Add documentation to configuration file
    Add documentation to configuration file

--- server/config/index.js
+++ server/config/index.js
@@ -1,13 +1,21 @@
 const config = {};
 
 config.jose = {
+    // key type
     kty: "EC",
+    // curve
     crvOrSize: "P-256",
+    // key derivation algorithm
     alg: "ECDH-ES+A128KW",
+    // symmetric encryption algorithm
     enc: "A128GCM",
+    // signing algorithm
     sigAlg: "ES256",
+    // issuer
     iss: "https://example.com",
+    // token expiry
     exp: "15 m",
+    // audience
     aud: "XYClient",
 };
 
